b167ac6686bfee7813f57ebeb615806948b151e7
aadityamp01 <51539518+aadityamp01@users.noreply.github.com>
2019-11-30 17:09:56 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,3 +1,5 @@
+#A Python program which accepts the radius of a circle from the user and compute the area.
+
 radius = float(input("Enter radius of circle::"))
 pi = 3.1415926536
 area = pi*(radius**2)
