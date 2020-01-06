ac4b27e0582f264be60ca08e8898756cf4821a5b
Aaditya Paithane <51539518+aadityamp01@users.noreply.github.com>
2020-01-06 23:12:36 +0530

    Delete task2.py

--- task2.py
+++
@@ -1,5 +0,0 @@
-# Python program to accept a filename from the user and print the extension of that file
-
-filename = input("Enter the Filename:: ")
-f_extension = filename.split(".")
-print ("The extension of the file is:: " + repr(f_extension[-1]))
